75f8e220502f2d88970449b5bff7d51f93af1e3b
TK-Linh <linhpony2007@gmail.com>
2026-06-16 15:05:04 +0700

    minor uart2 fixes

--- components/uart2_tlm/include/master_tb.h
+++ components/uart2_tlm/include/master_tb.h
@@ -7,7 +7,7 @@
 using namespace sc_core;
 
 
-#define ADDR_HOST_UART0 0x1A510000
+#define ADDR_HOST_UART0 0x10000000
 
 class MasterTB : public sc_module {
 public:
@@ -15,7 +15,7 @@ public:
 
     sc_in<unsigned char> host0_tx_mon;
     sc_core::sc_out<unsigned char> host0_rx_drv;
-    // SC_HAS_PROCESS(MasterTB);
+    SC_HAS_PROCESS(MasterTB);
     MasterTB(sc_module_name name);
 
 private:
