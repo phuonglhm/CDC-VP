3383321da5acd4ba3cda52cd9183cb83bd916fab
hieule275 <hieuhoangdinh2705@gmail.com>
2026-06-22 10:26:29 +0700

    Add OpenTitan-style PWM TLM model

--- components/pwm_tlm/pwm.cpp
+++ components/pwm_tlm/pwm.cpp
@@ -3,22 +3,54 @@
 
 PWM::PWM(sc_module_name name)
 : sc_module(name),
-  socket("socket")
+  socket("socket"),
+  pwm_out("pwm_out")
 {
     socket.register_b_transport(
         this,
-        &PWM::b_transport);
+        &PWM::b_transport
+    );
 
-    cfg         = 0;
-    pwm_en      = 0;
-    invert      = 0;
-    phase_delay = 0;
-    duty_cycle  = 0;
-    period      = 256;
+    alert_test = 0;
+    regwen     = 1;
 
-    counter     = 0;
+    // OpenTitan reset value of CFG is 0x38008000.
+    // CNTR_EN = 0, DC_RESN = 7, CLK_DIV = 0x8000.
+    cfg        = 0x38008000;
+
+    pwm_en     = 0;
+    invert     = 0;
+
+    for(int i = 0; i < 6; i++)
+    {
+        pwm_param[i]   = 0;
+        duty_cycle[i]  = 0;
+        blink_param[i] = 0;
+    }
+
+    counter = 0;
 
     SC_THREAD(pwm_thread);
+}
+
+uint64_t PWM::calc_period() const
+{
+    uint32_t dc_resn =
+        (cfg >> 27) & 0xF;
+
+    uint32_t clk_div =
+        cfg & 0x07FFFFFF;
+
+    uint64_t period =
+        (1ULL << (dc_resn + 1)) *
+        static_cast<uint64_t>(clk_div + 1);
+
+    if(period == 0)
+    {
+        period = 1;
+    }
+
+    return period;
 }
 
 void PWM::b_transport(
@@ -28,41 +60,191 @@ void PWM::b_transport(
     uint64_t addr =
         trans.get_address();
 
+    unsigned char* ptr =
+        trans.get_data_ptr();
+
     uint32_t* data =
-        reinterpret_cast<uint32_t*>(
-            trans.get_data_ptr());
+        reinterpret_cast<uint32_t*>(ptr);
+
+    if(trans.get_data_length() != 4)
+    {
+        trans.set_response_status(
+            TLM_BURST_ERROR_RESPONSE
+        );
+        return;
+    }
+
+    if(addr % 4 != 0)
+    {
+        trans.set_response_status(
+            TLM_ADDRESS_ERROR_RESPONSE
+        );
+        return;
+    }
 
     if(trans.is_write())
     {
         switch(addr)
         {
+        case REG_ALERT_TEST:
+            alert_test = *data;
+            break;
+
+        case REG_REGWEN:
+            regwen = *data & 0x1;
+            break;
+
         case REG_CFG:
-            cfg = *data;
+            if(regwen & 0x1)
+            {
+                cfg = *data;
+            }
             break;
 
         case REG_PWM_EN:
-            pwm_en = *data;
+            if(regwen & 0x1)
+            {
+                pwm_en = *data & 0x3F;
+            }
             break;
 
         case REG_INVERT:
-            invert = *data;
+            if(regwen & 0x1)
+            {
+                invert = *data & 0x3F;
+            }
             break;
 
         case REG_PWM_PARAM0:
-            phase_delay = *data;
+            if(regwen & 0x1)
+            {
+                pwm_param[0] = *data;
+            }
+            break;
+
+        case REG_PWM_PARAM1:
+            if(regwen & 0x1)
+            {
+                pwm_param[1] = *data;
+            }
+            break;
+
+        case REG_PWM_PARAM2:
+            if(regwen & 0x1)
+            {
+                pwm_param[2] = *data;
+            }
+            break;
+
+        case REG_PWM_PARAM3:
+            if(regwen & 0x1)
+            {
+                pwm_param[3] = *data;
+            }
+            break;
+
+        case REG_PWM_PARAM4:
+            if(regwen & 0x1)
+            {
+                pwm_param[4] = *data;
+            }
+            break;
+
+        case REG_PWM_PARAM5:
+            if(regwen & 0x1)
+            {
+                pwm_param[5] = *data;
+            }
             break;
 
         case REG_DUTY0:
-            duty_cycle = *data;
+            if(regwen & 0x1)
+            {
+                duty_cycle[0] = *data;
+            }
             break;
 
-        case REG_PERIOD:
-            period = *data;
+        case REG_DUTY1:
+            if(regwen & 0x1)
+            {
+                duty_cycle[1] = *data;
+            }
+            break;
+
+        case REG_DUTY2:
+            if(regwen & 0x1)
+            {
+                duty_cycle[2] = *data;
+            }
+            break;
+
+        case REG_DUTY3:
+            if(regwen & 0x1)
+            {
+                duty_cycle[3] = *data;
+            }
+            break;
+
+        case REG_DUTY4:
+            if(regwen & 0x1)
+            {
+                duty_cycle[4] = *data;
+            }
+            break;
+
+        case REG_DUTY5:
+            if(regwen & 0x1)
+            {
+                duty_cycle[5] = *data;
+            }
+            break;
+
+        case REG_BLINK_PARAM0:
+            if(regwen & 0x1)
+            {
+                blink_param[0] = *data;
+            }
+            break;
+
+        case REG_BLINK_PARAM1:
+            if(regwen & 0x1)
+            {
+                blink_param[1] = *data;
+            }
+            break;
+
+        case REG_BLINK_PARAM2:
+            if(regwen & 0x1)
+            {
+                blink_param[2] = *data;
+            }
+            break;
+
+        case REG_BLINK_PARAM3:
+            if(regwen & 0x1)
+            {
+                blink_param[3] = *data;
+            }
+            break;
+
+        case REG_BLINK_PARAM4:
+            if(regwen & 0x1)
+            {
+                blink_param[4] = *data;
+            }
+            break;
+
+        case REG_BLINK_PARAM5:
+            if(regwen & 0x1)
+            {
+                blink_param[5] = *data;
+            }
             break;
 
         default:
             trans.set_response_status(
-                TLM_ADDRESS_ERROR_RESPONSE);
+                TLM_ADDRESS_ERROR_RESPONSE
+            );
             return;
         }
     }
@@ -70,6 +252,14 @@ void PWM::b_transport(
     {
         switch(addr)
         {
+        case REG_ALERT_TEST:
+            *data = alert_test;
+            break;
+
+        case REG_REGWEN:
+            *data = regwen;
+            break;
+
         case REG_CFG:
             *data = cfg;
             break;
@@ -83,20 +273,81 @@ void PWM::b_transport(
             break;
 
         case REG_PWM_PARAM0:
-            *data = phase_delay;
+            *data = pwm_param[0];
+            break;
+
+        case REG_PWM_PARAM1:
+            *data = pwm_param[1];
+            break;
+
+        case REG_PWM_PARAM2:
+            *data = pwm_param[2];
+            break;
+
+        case REG_PWM_PARAM3:
+            *data = pwm_param[3];
+            break;
+
+        case REG_PWM_PARAM4:
+            *data = pwm_param[4];
+            break;
+
+        case REG_PWM_PARAM5:
+            *data = pwm_param[5];
             break;
 
         case REG_DUTY0:
-            *data = duty_cycle;
+            *data = duty_cycle[0];
             break;
 
-        case REG_PERIOD:
-            *data = period;
+        case REG_DUTY1:
+            *data = duty_cycle[1];
+            break;
+
+        case REG_DUTY2:
+            *data = duty_cycle[2];
+            break;
+
+        case REG_DUTY3:
+            *data = duty_cycle[3];
+            break;
+
+        case REG_DUTY4:
+            *data = duty_cycle[4];
+            break;
+
+        case REG_DUTY5:
+            *data = duty_cycle[5];
+            break;
+
+        case REG_BLINK_PARAM0:
+            *data = blink_param[0];
+            break;
+
+        case REG_BLINK_PARAM1:
+            *data = blink_param[1];
+            break;
+
+        case REG_BLINK_PARAM2:
+            *data = blink_param[2];
+            break;
+
+        case REG_BLINK_PARAM3:
+            *data = blink_param[3];
+            break;
+
+        case REG_BLINK_PARAM4:
+            *data = blink_param[4];
+            break;
+
+        case REG_BLINK_PARAM5:
+            *data = blink_param[5];
             break;
 
         default:
             trans.set_response_status(
-                TLM_ADDRESS_ERROR_RESPONSE);
+                TLM_ADDRESS_ERROR_RESPONSE
+            );
             return;
         }
     }
@@ -104,7 +355,8 @@ void PWM::b_transport(
     delay += sc_time(10, SC_NS);
 
     trans.set_response_status(
-        TLM_OK_RESPONSE);
+        TLM_OK_RESPONSE
+    );
 }
 
 void PWM::pwm_thread()
@@ -114,29 +366,48 @@ void PWM::pwm_thread()
         wait(10, SC_NS);
 
         bool counter_enable =
-            ((cfg >> 31) & 1);
+            ((cfg >> 31) & 0x1);
 
         if(!counter_enable)
+        {
+            pwm_out.write(false);
             continue;
+        }
+
+        if((pwm_en & 0x1) == 0)
+        {
+            pwm_out.write(false);
+            continue;
+        }
+
+        uint64_t period =
+            calc_period();
 
         counter =
             (counter + 1) % period;
 
-        bool pwm = false;
+        // Channel 0 only.
+        // DUTY_CYCLE_0[15:0] = A.
+        // DUTY_CYCLE_0[31:16] = B.
+        // Normal mode uses A.
+        uint16_t duty_a =
+            duty_cycle[0] & 0xFFFF;
 
-        if(pwm_en & 0x1)
-        {
-            uint16_t shifted =
-                (counter +
-                 period -
-                 phase_delay)
-                 % period;
+        uint64_t duty_count =
+            (static_cast<uint64_t>(duty_a) * period) >> 16;
 
-            if(shifted < duty_cycle)
-            {
-                pwm = true;
-            }
-        }
+        // PWM_PARAM_0[15:0] = PHASE_DELAY.
+        uint16_t phase_raw =
+            pwm_param[0] & 0xFFFF;
+
+        uint64_t phase_count =
+            (static_cast<uint64_t>(phase_raw) * period) >> 16;
+
+        uint64_t shifted =
+            (counter + period - phase_count) % period;
+
+        bool pwm =
+            shifted < duty_count;
 
         if(invert & 0x1)
         {
@@ -145,12 +416,22 @@ void PWM::pwm_thread()
 
         pwm_out.write(pwm);
 
-        // std::cout
-        //     << sc_time_stamp()
-        //     << " counter="
-        //     << counter
-        //     << " pwm="
-        //     << pwm
-        //     << std::endl;
+        std::cout
+            << sc_time_stamp()
+            << " counter="
+            << counter
+            << " period="
+            << period
+            << " duty_raw=0x"
+            << std::hex
+            << duty_a
+            << std::dec
+            << " duty_count="
+            << duty_count
+            << " phase_count="
+            << phase_count
+            << " pwm="
+            << pwm
+            << std::endl;
     }
 }
--- components/pwm_tlm/tb.cpp
+++ components/pwm_tlm/tb.cpp
@@ -1,6 +1,8 @@
 #include <systemc>
 #include <tlm>
 #include <tlm_utils/simple_initiator_socket.h>
+#include <iostream>
+#include <cstdint>
 
 #include "pwm.h"
 
@@ -10,9 +12,7 @@ using namespace tlm;
 class TB : public sc_module
 {
 public:
-
-    tlm_utils::simple_initiator_socket<TB>
-        socket;
+    tlm_utils::simple_initiator_socket<TB> socket;
 
     SC_HAS_PROCESS(TB);
 
@@ -23,9 +23,7 @@ public:
         SC_THREAD(run);
     }
 
-    void write_reg(
-        uint32_t addr,
-        uint32_t value)
+    void write_reg(uint32_t addr, uint32_t value)
     {
         tlm_generic_payload trans;
 
@@ -33,90 +31,181 @@ public:
             SC_ZERO_TIME;
 
         trans.set_command(
-            TLM_WRITE_COMMAND);
+            TLM_WRITE_COMMAND
+        );
 
         trans.set_address(addr);
 
         trans.set_data_ptr(
-            reinterpret_cast<unsigned char*>(
-                &value));
+            reinterpret_cast<unsigned char*>(&value)
+        );
 
         trans.set_data_length(4);
 
         socket->b_transport(
             trans,
-            delay);
+            delay
+        );
+
+        if(trans.get_response_status() != TLM_OK_RESPONSE)
+        {
+            std::cout
+                << "WRITE ERROR addr=0x"
+                << std::hex
+                << addr
+                << " value=0x"
+                << value
+                << std::dec
+                << std::endl;
+        }
+
+        wait(delay);
+    }
+
+    uint32_t read_reg(uint32_t addr)
+    {
+        tlm_generic_payload trans;
+
+        sc_time delay =
+            SC_ZERO_TIME;
+
+        uint32_t value = 0;
+
+        trans.set_command(
+            TLM_READ_COMMAND
+        );
+
+        trans.set_address(addr);
+
+        trans.set_data_ptr(
+            reinterpret_cast<unsigned char*>(&value)
+        );
+
+        trans.set_data_length(4);
+
+        socket->b_transport(
+            trans,
+            delay
+        );
+
+        if(trans.get_response_status() != TLM_OK_RESPONSE)
+        {
+            std::cout
+                << "READ ERROR addr=0x"
+                << std::hex
+                << addr
+                << std::dec
+                << std::endl;
+        }
+
+        wait(delay);
+
+        return value;
     }
 
     void run()
     {
+        const uint32_t REG_CFG        = 0x08;
+        const uint32_t REG_PWM_EN     = 0x0C;
+        const uint32_t REG_INVERT     = 0x10;
+        const uint32_t REG_PWM_PARAM0 = 0x14;
+        const uint32_t REG_DUTY0      = 0x2C;
+
+        const uint32_t CFG_CNTR_EN    = 1u << 31;
+
+        // DC_RESN = 7 gives period = 2^(7 + 1) * (CLK_DIV + 1)
+        // CLK_DIV = 0 gives period = 256 ticks.
+        const uint32_t DC_RESN_7      = 7u << 27;
+        const uint32_t CLK_DIV_0      = 0u;
+
         wait(100, SC_NS);
 
-    std::cout << "\n========== TC1: PWM Disable ==========\n";
+        std::cout
+            << "\n========== Read reset registers ==========\n";
 
-    write_reg(0x08, 0x80000000); // counter enable
-    write_reg(0x0C, 0);          // pwm disable
-    write_reg(0x10, 0);
-    write_reg(0x14, 0);
-    write_reg(0x2C, 40);
-    write_reg(0x30, 100);
+        std::cout
+            << "CFG reset = 0x"
+            << std::hex
+            << read_reg(REG_CFG)
+            << std::dec
+            << std::endl;
 
-    wait(1000, SC_NS);
+        std::cout
+            << "\n========== TC1: Counter enable, PWM channel disable ==========\n";
 
+        write_reg(REG_CFG, CFG_CNTR_EN | DC_RESN_7 | CLK_DIV_0);
+        write_reg(REG_PWM_EN, 0x0);
+        write_reg(REG_INVERT, 0x0);
+        write_reg(REG_PWM_PARAM0, 0x00000000);
+        write_reg(REG_DUTY0, 0x00008000); // 50%
 
-    std::cout << "\n========== TC2: Duty 40% ==========\n";
+        wait(1000, SC_NS);
 
-    write_reg(0x0C, 1);          // pwm enable
-    write_reg(0x10, 0);
-    write_reg(0x14, 0);
-    write_reg(0x2C, 40);
-    write_reg(0x30, 100);
+        std::cout
+            << "\n========== TC2: Duty 40% ==========\n";
 
-    wait(1000, SC_NS);
+        write_reg(REG_PWM_EN, 0x1);
+        write_reg(REG_INVERT, 0x0);
+        write_reg(REG_PWM_PARAM0, 0x00000000);
 
+        // 40% of 0x10000 approximately = 0x6666
+        write_reg(REG_DUTY0, 0x00006666);
 
-    std::cout << "\n========== TC3: Duty 75% ==========\n";
+        wait(3000, SC_NS);
 
-    write_reg(0x2C, 75);
+        std::cout
+            << "\n========== TC3: Duty 75% ==========\n";
 
-    wait(1000, SC_NS);
+        // 75% = 0xC000
+        write_reg(REG_DUTY0, 0x0000C000);
 
+        wait(3000, SC_NS);
 
-    std::cout << "\n========== TC4: Phase Delay 20 ==========\n";
+        std::cout
+            << "\n========== TC4: Phase delay 20% ==========\n";
 
-    write_reg(0x14, 20);
+        // 20% of 0x10000 approximately = 0x3333
+        write_reg(REG_PWM_PARAM0, 0x00003333);
 
-    wait(1000, SC_NS);
+        wait(3000, SC_NS);
 
+        std::cout
+            << "\n========== TC5: Invert ==========\n";
 
-    std::cout << "\n========== TC5: Invert ==========\n";
+        write_reg(REG_INVERT, 0x1);
 
-    write_reg(0x10, 1);
+        wait(3000, SC_NS);
 
-    wait(1000, SC_NS);
+        std::cout
+            << "\n========== TC6: Duty 0% ==========\n";
 
+        write_reg(REG_INVERT, 0x0);
+        write_reg(REG_PWM_PARAM0, 0x00000000);
+        write_reg(REG_DUTY0, 0x00000000);
 
-    std::cout << "\n========== TC6: Duty 0% ==========\n";
+        wait(3000, SC_NS);
 
-    write_reg(0x10, 0);
-    write_reg(0x2C, 0);
+        std::cout
+            << "\n========== TC7: Duty nearly 100% ==========\n";
 
-    wait(1000, SC_NS);
+        // OpenTitan 16-bit duty max is 0xFFFF.
+        // It is almost 100%.
+        write_reg(REG_DUTY0, 0x0000FFFF);
 
+        wait(3000, SC_NS);
 
-    std::cout << "\n========== TC7: Duty 100% ==========\n";
+        std::cout
+            << "\n========== TC8: Disable counter ==========\n";
 
-    write_reg(0x2C, 100);
+        write_reg(REG_CFG, 0x0);
 
-    wait(1000, SC_NS);
+        wait(1000, SC_NS);
 
-    sc_stop();
+        sc_stop();
     }
 };
 
-int sc_main(
-    int argc,
-    char* argv[])
+int sc_main(int argc, char* argv[])
 {
     PWM pwm("PWM");
 
@@ -127,23 +216,27 @@ int sc_main(
     pwm.pwm_out(pwm_sig);
 
     tb.socket.bind(
-        pwm.socket);
+        pwm.socket
+    );
 
     sc_trace_file* tf;
 
     tf =
-      sc_create_vcd_trace_file(
-          "wave");
+        sc_create_vcd_trace_file(
+            "wave"
+        );
 
     sc_trace(
         tf,
         pwm_sig,
-        "pwm_out");
+        "pwm_out"
+    );
 
     sc_start();
 
     sc_close_vcd_trace_file(
-        tf);
+        tf
+    );
 
     return 0;
 }
