4421cf62718e459527bcac51f61f5f7b344d3726
hvu <hoang040507@gmail.com>
2026-06-26 13:46:16 +0700

    Add isp_tlm skeleton

---
+++ components/isp_tlm/src/isp_tlm.cpp
@@ -0,0 +1,102 @@
+#include "adc_tlm.h"
+
+#include <cstdint>
+#include <cstring>
+
+namespace cdc::components {
+
+adc_tlm::adc_tlm(sc_core::sc_module_name name, sc_core::sc_time access_latency)
+    : sc_core::sc_module(name)
+    , socket("socket")
+    , reset_n("reset_n")
+    , irq_out("irq_out")
+    , access_latency_(access_latency)
+{
+    socket.register_b_transport(this, &adc_tlm::b_transport);
+    socket.register_transport_dbg(this, &adc_tlm::transport_dbg);
+
+    SC_METHOD(drive_outputs);
+    sensitive << reset_n << irq_update_event_;
+    dont_initialize();
+}
+
+void adc_tlm::start_of_simulation()
+{
+    if (!reset_n.read()) {
+        core_.reset();
+    }
+    irq_level_ = false;
+    irq_out.write(false);
+}
+
+void adc_tlm::drive_outputs()
+{
+    if (!reset_n.read()) {
+        core_.reset();
+        irq_level_ = false;
+    }
+    irq_out.write(irq_level_);
+}
+
+void adc_tlm::update_irq()
+{
+    irq_level_ = core_.hasInterrupt();
+    irq_update_event_.notify(sc_core::SC_ZERO_TIME);
+}
+
+void adc_tlm::b_transport(tlm::tlm_generic_payload& trans, sc_core::sc_time& delay)
+{
+    delay += access_latency_;
+
+    const std::uint64_t offset = trans.get_address();
+    const unsigned len = trans.get_data_length();
+    unsigned char* ptr = trans.get_data_ptr();
+
+    if (len != sizeof(std::uint32_t) || (offset & 0x3u) != 0u || offset > 0x0Cu || ptr == nullptr) {
+        trans.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
+        return;
+    }
+
+    if (trans.get_command() == tlm::TLM_READ_COMMAND) {
+        const std::uint32_t value = core_.readReg(static_cast<std::uint32_t>(offset));
+        std::memcpy(ptr, &value, sizeof(value));
+    } else if (trans.get_command() == tlm::TLM_WRITE_COMMAND) {
+        std::uint32_t value = 0;
+        std::memcpy(&value, ptr, sizeof(value));
+        core_.writeReg(static_cast<std::uint32_t>(offset), value);
+    } else {
+        trans.set_response_status(tlm::TLM_COMMAND_ERROR_RESPONSE);
+        return;
+    }
+
+    update_irq();
+    trans.set_response_status(tlm::TLM_OK_RESPONSE);
+}
+
+unsigned int adc_tlm::transport_dbg(tlm::tlm_generic_payload& trans)
+{
+    const std::uint64_t offset = trans.get_address();
+    const unsigned len = trans.get_data_length();
+    unsigned char* ptr = trans.get_data_ptr();
+
+    if (len != sizeof(std::uint32_t) || (offset & 0x3u) != 0u || offset > 0x0Cu || ptr == nullptr) {
+        trans.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
+        return 0;
+    }
+
+    if (trans.get_command() == tlm::TLM_READ_COMMAND) {
+        const std::uint32_t value = core_.debugReadReg(static_cast<std::uint32_t>(offset));
+        std::memcpy(ptr, &value, sizeof(value));
+    } else if (trans.get_command() == tlm::TLM_WRITE_COMMAND) {
+        std::uint32_t value = 0;
+        std::memcpy(&value, ptr, sizeof(value));
+        core_.debugWriteReg(static_cast<std::uint32_t>(offset), value);
+    } else {
+        return 0;
+    }
+
+    trans.set_response_status(tlm::TLM_OK_RESPONSE);
+    return sizeof(std::uint32_t);
+}
+
+} // namespace cdc::components
