4c1b6547d693cbece75bce139a24f2a5fca401a3
hvu <hoang040507@gmail.com>
2026-06-30 10:12:14 +0700

    isp: implement DG block

--- components/isp_tlm/include/blocks/dg.h
+++ components/isp_tlm/include/blocks/dg.h
@@ -4,14 +4,16 @@
 #include <stdint.h>
 
 struct dg_config {
-    bool is_auto;
-    uint16_t current_gain;
-    int32_t ae_feedback;
+   bool is_enable;
+   bool is_auto;
+   uint16_t current_gain;
+   int32_t ae_feedback;
 };
 
 class dg_block {
 public:
-    void process(const uint16_t* in, uint16_t* out, uint32_t w, uint32_t h, const dg_config& cfg, uint8_t bit_depth);
+   void process(
+       const uint16_t *in, uint16_t *out, uint32_t w, uint32_t h, const dg_config &cfg, uint8_t bit_depth);
 };
 
 #endif // DG_BLOCK_H
